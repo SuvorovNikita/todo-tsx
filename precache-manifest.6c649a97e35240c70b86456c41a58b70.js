bd834e55be24733c05a956bbb3179f1c7b734304
Никита <nikit.suvorov@gmail.com>
2020-05-06 19:39:58 +0300

    Updates

---
+++ precache-manifest.6c649a97e35240c70b86456c41a58b70.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06fd5ec04783279d0cc97572b904d3e3",
+    "url": "/todo-tsx/index.html"
+  },
+  {
+    "revision": "1677b47692b32ed1b5a2",
+    "url": "/todo-tsx/static/css/main.e8c4e865.chunk.css"
+  },
+  {
+    "revision": "fe760e4f61850b411437",
+    "url": "/todo-tsx/static/js/2.19e14fa2.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/todo-tsx/static/js/2.19e14fa2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1677b47692b32ed1b5a2",
+    "url": "/todo-tsx/static/js/main.56226cd4.chunk.js"
+  },
+  {
+    "revision": "14d145ba94c1dda34308",
+    "url": "/todo-tsx/static/js/runtime-main.cc3307b1.js"
+  }
+]);
